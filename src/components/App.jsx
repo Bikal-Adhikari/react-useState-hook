78fbd1189f6492a320aa73ad5019a1117fa59842
Bikal <bikaladhikari5@gmail.com>
2024-03-29 17:41:27 +1100

    Use state count complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 function App() {
-  const [isClicked, setIsClicked] = useState();
+  const [count, setCount] = useState(0);
 
-  const increase =()=>{
-    
-  }
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count - 1);
+  };
   return (
     <div className="container">
-      <h1>0</h1>
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
       <button onClick={increase}>+</button>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-var count = 0;
-
-const increase = () => {};
-
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
